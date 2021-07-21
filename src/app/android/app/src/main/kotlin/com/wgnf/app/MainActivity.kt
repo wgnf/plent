b4223f0c32a4721137263596164a7124cbd7ba47
Martin Wagenführ <martin@wgnf.de>
2021-07-21 20:17:52 +0200

    Initializes empty Flutter-Project

---
+++ src/app/android/app/src/main/kotlin/com/wgnf/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wgnf.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ src/app/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // nothing for now lol
+}
