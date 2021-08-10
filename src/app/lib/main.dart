31ce9bdd0006de9be3c75428a6298f86c0083776
Martin Wagenführ <martin@wgnf.de>
2021-08-10 22:40:03 +0200

    Sets default locale to 'en'

--- src/app/lib/main.dart
+++ src/app/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:intl/intl.dart';
 import 'package:plent/views/plent_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  Intl.defaultLocale = 'en';
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(PlentApp());
